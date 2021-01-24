a304082218b864feee51e6c58345825a4d56466e
yeemans <arthur.roth.07@gmail.com>
2021-01-24 11:01:10 -0800

    testing partials

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,10 +1,14 @@
 class CarsController < ApplicationController
   before_action :set_car, only: [:show, :edit, :update, :destroy]
 
+  def test_render 
+    
+  end
   # GET /cars
   # GET /cars.json
   def index
     @cars = Car.all
+    test_render
   end
 
   # GET /cars/1
